e0ead45116a828e342146b6bc06bd49647d7dacc
Ceaseless04 <kristian120304@gmail.com>
2023-02-23 13:55:26 -0500

    added tech skills subheadings

--- test/src/App.js
+++ test/src/App.js
@@ -5,6 +5,7 @@ export default function App() {
     <div className="App">
       <body>
           <div className="text">
+            {/*nav bar that links according to headings*/}
             <div className="nav-bar">
               <a class="active" href=".about-me">About Me</a>
               <a href=".education">Education</a>
@@ -15,12 +16,14 @@ export default function App() {
             <h2 id="intro">Hi!😄</h2>
             <div className="about-me">
               <h2 id="intro">About Me</h2>
+              {/*Short description (interest other than coding)*/}
               <p className="description">
                 Apart from being a Software Engineer,<br></br>
                 I like to listen to music, read books,<br></br>
                 and help my community.
               </p>
             </div>
+            {/*Education relevant to FIU (courses that relate to CS)*/}
             <div className="education">
               <h2 id="intro">Education</h2>
               <h3 id="school">Florida International University</h3>
@@ -35,10 +38,18 @@ export default function App() {
                   </ul> 
                 </p>
             </div>
+            {/*Dividing skills in 3 categories: tech skills, soft skills, and dev tools*/}
             <div className="tech-skills">
               <h2 id="intro">Technical Skills</h2>
               <div className="typesOf-skills">
-                <h3 id="coding-languages">Languages</h3>
+                <div className="coding-languages">
+                  <h3 id="languages">Languages</h3>
+                  <ul>
+                    <li>
+                      
+                    </li>
+                  </ul>
+                </div>
                 <h3 id="database">Database Management</h3>
                 <h3 id="tools">Tools</h3>
                 <h3 id="other">Soft Skills</h3>
