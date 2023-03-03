6b00eabcef88eaacf93a6bca8ac2bdaf7f117092
Ceaseless04 <kristian120304@gmail.com>
2023-03-03 16:11:23 -0500

    created navbar and edited background color

--- test/src/App.js
+++ test/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <TopNavBar />
     </div>
   );
 }
 
-export default App;
+function TopNavBar() {
+  return (
+    <nav className="text-center text-lg overflow-hidden no-underline py-3.5 px-4">
+      <a href='#about' className="py-3.5 px-4">About</a>
+      <a href='#skills' className="py-3.5 px-4">Skills</a>
+      <a href='#portfolio' className="py-3.5 px-4">Portfolio</a>
+      <a href='#contact' className="py-3.5 px-4">Contact</a>
+    </nav>
+  );
+}
