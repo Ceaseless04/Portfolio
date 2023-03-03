cb5f96e99ab33b844c657b85b33a8c6ea1730622
Ceaseless04 <kristian120304@gmail.com>
2023-03-03 16:50:07 -0500

    added just-in-time mode to tailwind.config.js

--- test/tailwind.config.js
+++ test/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit', // added just-in-time mode, making the builds faster
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
