ffc75ddbd400767cb5222414f5a8b15deea2a636
Ceaseless04 <kristian120304@gmail.com>
2023-03-03 13:33:33 -0500

    not using tailwind in the meantime

--- test/src/App.js
+++ test/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div>
+        <a href='#about'>About</a>
+        <a href='#skills'>Skills</a>
+        <a href='#portfolio'>Portfolio</a>
+        <a href='#contact'>Contact</a>
+      </div>
+    </body>
   );
 }
-
-export default App;
