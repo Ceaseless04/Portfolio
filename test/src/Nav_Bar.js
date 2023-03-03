4a76a7de6feb534bde60d408b92d594c29b3f0b4
Ceaseless04 <kristian120304@gmail.com>
2023-03-03 17:14:30 -0500

    styled the top nav bar

--- test/src/Nav_Bar.js
+++ test/src/Nav_Bar.js
@@ -1,8 +1,10 @@
 export default function TopNavBar() {
     return (
-      <nav className="text-center text-lg overflow-hidden no-underline py-3.5 px-4">
+      <nav className="text-center text-lg overflow-hidden no-underline py-3.5 px-4 bg-red-100 w-96 mx-auto rounded-b-2xl">
         <a href='#about' className="py-3.5 px-4">About</a>
-        <a href='#skills' className="py-3.5 px-4">Skills</a>
+        <button>
+            <a href='#skills' className="py-3.5 px-4">Skills</a>
+        </button>
         <a href='#portfolio' className="py-3.5 px-4">Portfolio</a>
         <a href='#contact' className="py-3.5 px-4">Contact</a>
       </nav>
