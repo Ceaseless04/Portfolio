145fd2d6bf7d9050035ab339723e3ee19b7540be
Ceaseless04 <kristian120304@gmail.com>
2023-03-03 16:56:51 -0500

    did some organizing with the navbar function to the main function

--- test/src/App.js
+++ test/src/App.js
@@ -1,20 +1,9 @@
-import './App.css';
+import TopNavBar from './Nav_Bar';
 
 export default function App() {
   return (
     <div>
         <TopNavBar />
     </div>
-  );
-}
-
-function TopNavBar() {
-  return (
-    <nav className="text-center text-lg overflow-hidden no-underline py-3.5 px-4">
-      <a href='#about' className="py-3.5 px-4">About</a>
-      <a href='#skills' className="py-3.5 px-4">Skills</a>
-      <a href='#portfolio' className="py-3.5 px-4">Portfolio</a>
-      <a href='#contact' className="py-3.5 px-4">Contact</a>
-    </nav>
   );
 }
---
+++ test/src/Nav_Bar.js
@@ -0,0 +1,10 @@
+export default function TopNavBar() {
+    return (
+      <nav className="text-center text-lg overflow-hidden no-underline py-3.5 px-4">
+        <a href='#about' className="py-3.5 px-4">About</a>
+        <a href='#skills' className="py-3.5 px-4">Skills</a>
+        <a href='#portfolio' className="py-3.5 px-4">Portfolio</a>
+        <a href='#contact' className="py-3.5 px-4">Contact</a>
+      </nav>
+    );
+  }
