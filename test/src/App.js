6f243413246da95757dfd9b7fcbbafe5037ab737
Ceaseless04 <kristian120304@gmail.com>
2023-03-03 14:08:20 -0500

    starting over

--- test/src/App.js
+++ test/src/App.js
@@ -1,14 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
 
-export default function App() {
+function App() {
   return (
-    <body>
-      <div>
-        <a href='#about'>About</a>
-        <a href='#skills'>Skills</a>
-        <a href='#portfolio'>Portfolio</a>
-        <a href='#contact'>Contact</a>
-      </div>
-    </body>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
+export default App;
