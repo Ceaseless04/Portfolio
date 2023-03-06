d8bfceb3012bc0b3f684d44a31de6c1b6c8a4cb9
Ceaseless04 <kristian120304@gmail.com>
2023-03-06 15:51:33 -0500

    worked on styling

--- test/src/About.js
+++ test/src/About.js
@@ -3,19 +3,21 @@ import IMG_0094 from './IMG_0094.png';
 export default function About() {
     return (
         <div id='about'>
-            <img src={ IMG_0094 } alt='Me' className='absolute w-auto h-96 rounded-lg mx-72 mt-4'></img>
-            <span>
-                <h2 className='relative text-right text-3xl pr-96 pt-4'>About Me</h2>
-                <br></br>
-                <p className='relative text-right text-l pr-80'>
-                    Besides being a Software Engineer,<br></br> I like to watch movies, skateboard,<br></br>
-                     work-out, and help my community.
-                </p>
+            {/* <img src={ IMG_0094} alt='Me' className='absolute w-auto h-96 rounded-lg mx-72 mt-4'></img> */}
+            <span className=''>
+                <img src={ IMG_0094} alt='Me' className='absolute w-auto h-96 rounded-lg mx-72 mt-4'></img>
+                <h2 className='relative text-right text-3xl w-auto pr-96 pt-4'>About Me</h2>
                 <br></br>
                 <p className='relative text-center text-l pl-96'>
-                    I am currently pursuing a Bachelors in Computer Science,<br></br>
-                    being enrolled in courses such as Programming II and<br></br> 
-                    Discrete Structures.
+                    Besides being a Software Engineer, I like to watch movies, skateboard,
+                     work-out, 
+                     <br></br>
+                     and help my community.
+                    <br></br>
+                    I am currently pursuing a Bachelors in Computer Science,
+                    being enrolled in courses such as 
+                    <br></br>
+                    Programming II and Discrete Structures.
                 </p>
             </span>
         </div>
