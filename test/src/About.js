0714384c84150b332a9800305ed946d4f2be80cb
Ceaseless04 <kristian120304@gmail.com>
2023-03-07 15:08:39 -0500

    added new branch for prototype

--- test/src/About.js
+++ test/src/About.js
@@ -2,11 +2,14 @@ import IMG_0094 from './IMG_0094.png';
 
 export default function About() {
     return (
-        <div id='about'>
+        <div id='about' className='flex'>
             {/* <img src={ IMG_0094} alt='Me' className='absolute w-auto h-96 rounded-lg mx-72 mt-4'></img> */}
-            <span className=''>
-                <img src={ IMG_0094} alt='Me' className='absolute w-auto h-96 rounded-lg mx-72 mt-4'></img>
-                <h2 className='relative text-right text-3xl w-auto pr-96 pt-4'>About Me</h2>
+                <div>
+                    <img src={ IMG_0094 } alt='Me' className='object-cover h-96 w-96'></img>
+                </div>
+                <div>
+                    <h2 className='relative text-right text-3xl w-auto pr-96 pt-4'>About Me</h2>
+                </div>
                 <br></br>
                 <p className='relative text-center text-l pl-96'>
                     Besides being a Software Engineer, I like to watch movies, skateboard,
@@ -20,7 +23,6 @@ export default function About() {
                     Programming II and Discrete Structures.
                 </p>
                 <br></br>
-            </span>
         </div>
     );
 }
