afe83f6a14da2764af616c85b74d9d6ff6c13c94
Ceaseless04 <kristian120304@gmail.com>
2023-03-07 11:00:14 -0500

    working on the skills.js file

--- test/src/About.js
+++ test/src/About.js
@@ -19,6 +19,7 @@ export default function About() {
                     <br></br>
                     Programming II and Discrete Structures.
                 </p>
+                <br></br>
             </span>
         </div>
     );
--- test/src/App.js
+++ test/src/App.js
@@ -1,11 +1,13 @@
 import TopNavBar from './Nav_Bar';
 import About from './About';
+import Skills from './Skills';
 
 export default function App() {
   return (
     <div>
         <TopNavBar />
         <About />
+        <Skills />
     </div>
   );
 }
