2f50792b9ffed00a85c9256467bdf200f9a9e966
Ceaseless04 <kristian120304@gmail.com>
2023-03-09 01:09:07 -0500

    made new branch and need to work on the flexbox for about me

--- test/src/Skills.js
+++ test/src/Skills.js
@@ -2,10 +2,20 @@ export default function Skills() {
     return (
         <div id='skills' className="text-center text-3xl pt-72">
             <h2>Skills</h2>
-            <div className="grid-rows-1 columns-3 pt-8">
-                <h2>
-
-                </h2>
+            <div className="grid-rows-1 columns-3 pt-8 text-2xl">
+                {/* Dev tools container */}
+                <h5>Dev Tools</h5>
+                <h5>Languages</h5>
+                <h5>Soft Skills</h5>
+            </div>
+            <div className="text-center text-base">
+                <ul className="list-disc">
+                    <li>VS Code</li>
+                    <li>Github</li>
+                    <li>Firebase</li>
+                    <li>Git</li>
+                    <li>IntelliJ</li>
+                </ul>
             </div>
         </div>
     );
