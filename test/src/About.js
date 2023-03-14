0df9164d3db7ec1780fbf77d487509d42e1c8e55
Ceaseless04 <kristian120304@gmail.com>
2023-03-14 00:24:04 -0400

    finished styling the about me page

--- test/src/About.js
+++ test/src/About.js
@@ -2,12 +2,12 @@ import IMG_0094 from './Images/IMG_0094.png';
 
 export default function About() {
     return (
-        <div id='about' className='grid-row-1 columns-2xs'>
+        <div id='about' className='flex items-center justify-center grid-row-1 columns-2xs pt-11 space-x-11'>
                 <div className="flex justify-center">
                     <img src={ IMG_0094 } alt='Me'className='rounded-lg w-auto h-96'></img>
                 </div>
-                <div className='text-center text-l'>
-                    <h2 className='text-center text-3xl w-auto '>About Me</h2>
+                <div className='text-center text-l grid-rows-2 space-y-3'>
+                    <h2 className='text-3xl w-auto'>About Me</h2>
                     <p>
                         Besides being a Software Engineer, I like to watch movies, skateboard,
                         work-out, 
@@ -16,7 +16,8 @@ export default function About() {
                         <br></br>
                         <br></br>
                         I am currently pursuing a Bachelors in Computer Science,
-                        being enrolled in courses such as Programming II and Discrete Structures.
+                        being enrolled
+                        <br></br> in courses such as Programming II and Discrete Structures.
                     </p>
                     <br></br>
                 </div>
