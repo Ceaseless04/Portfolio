4798c437af8b77f76dcc0985a0e3ef0e49665c4f
Ceaseless04 <kristian120304@gmail.com>
2023-03-13 22:46:23 -0400

    will add react router

--- test/src/Skills.js
+++ test/src/Skills.js
@@ -2,14 +2,14 @@ export default function Skills() {
     return (
         <div id='skills' className="text-center pt-32">
             <h2 className="text-3xl">Skills</h2>
-            <div className="grid-rows-1 columns-5 pt-8 text-2xl">
+            <div className="grid-rows-1 columns-3 pt-8 text-2xl">
                 {/* Dev tools container */}
                 <h5>Dev Tools</h5>
                 <h5>Languages</h5>
                 <h5>Soft Skills</h5>
             </div>
             <div className="flex justify-center text-base text-center">
-                <ul className="list-disc list-inside grid-rows-1 columns-3 inline-block text-left">
+                <ul className="list-disc list-inside grid-rows-1 columns-3 inline-block text-left space-x-28">
                     {/* Dev Tools list */}
                     <div>
                         <li>VS Code</li>
