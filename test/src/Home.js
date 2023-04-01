368a250868a058faa941acc721c5c1757d2e1819
Ceaseless04 <kristian120304@gmail.com>
2023-03-31 23:55:58 -0400

    added contact icon, will move to top corner of page
    need to start on the projects and contacts pages
    
    WORK on navigation cases for hrefs skills and about

--- test/src/Home.js
+++ test/src/Home.js
@@ -20,15 +20,17 @@ export const NavBar = () => {
   return (
     <>
     {/* nav tag for the navigation bar */}
-    <nav className="top-0 text-center text-lg overflow-hidden no-underline bg-red-200 drop-shadow-lg py-3.5 px-3.5 w-max mx-auto rounded-b-2xl font-a">
-              <div className="container flex flex-wrap items-center justify-between mx-auto">
-              {/* will be linked to the About me section in another function*/}
-                  <button onClick= {() => navigate("/")} className="py-3.5 px-4 hover:bg-red-300 rounded-full">Home</button>
-                  <a href='#about' src={About} className="py-3.5 px-4 hover:bg-red-300 rounded-full">About</a>
-                  <button onClick={() => navigate("/Projects")} className="py-3.5 px-4 hover:bg-red-300 rounded-full">Projects</button>
-                  <a href="#skills" src={Skills} className="py-3.5 px-4 hover:bg-red-300 rounded-full">Skills</a>
-                  <button onClick={() => navigate("/Contact")} className="py-3.5 px-3.5 inline-flex hover:bg-red-300 rounded-full"><img src={Photo} alt='Me drinking Boba' className='object-scale-down h-10 max-w-full rounded-full'></img></button>
-              </div>
+    <nav>
+      <div className="container flex flex-wrap items-center justify-between mx-auto bg-red-200 top-0 text-center text-lg overflow-hidden no-underline drop-shadow-lg py-3.5 px-3.5 w-max rounded-b-2xl font-a">
+      {/* will be linked to the About me section in another function*/}
+          <button onClick= {() => navigate("/")} className="py-3.5 px-4 hover:bg-red-300 rounded-full">Home</button>
+          <a href='#about' src={About} className="py-3.5 px-4 hover:bg-red-300 rounded-full">About</a>
+          <button onClick={() => navigate("/Projects")} className="py-3.5 px-4 hover:bg-red-300 rounded-full">Projects</button>
+          <a href="#skills" src={Skills} className="py-3.5 px-4 hover:bg-red-300 rounded-full">Skills</a>
+      </div>
+      <div className=''>
+        <button onClick={() => navigate("/Contact")} className="inline-flex rounded-full border-4 border-red-200 hover:scale-110"><img src={Photo} alt='Me drinking Boba' className='object-scale-down h-24 max-w-full rounded-full'></img></button>
+      </div>
     </nav>
     </>
   );
