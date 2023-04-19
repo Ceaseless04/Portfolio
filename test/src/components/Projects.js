ef06e7357782fd6c3842d3694baf98d44b45b509
Ceaseless04 <kristian120304@gmail.com>
2023-04-19 00:05:56 -0400

    did some cleanup around the skills card
    -changed contact page to an anchor tag instead of a react router button
    
    -will keep projects page as react router button
    
    make contact page card content

--- test/src/components/Projects.js
+++ test/src/components/Projects.js
@@ -1,4 +1,16 @@
+import React, { useState } from 'react';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RxDotFilled } from 'react-icons/rx';
+
+
 export default function Projects() {
+    const projects_slides = [
+        {
+            'url': ,
+        },
+    ];
+
+
     return (
         <>
         </>
