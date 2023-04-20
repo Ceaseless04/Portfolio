7ad4d123cc6f26e59fb8fa66846e8b37f7fc0d5b
Ceaseless04 <kristian120304@gmail.com>
2023-04-19 21:16:45 -0400

    did some cleanup around the skills card
    -changed contact page to an anchor tag instead of a react router button
    
    -will keep projects page as react router button
    
    make contact page card content

--- test/src/App.js
+++ test/src/App.js
@@ -7,15 +7,13 @@ export default function App() {
   return (
     <>
       <BrowserRouter> {/* uses history API to keep UI in sync with URL*/}
-      <div>
         <NavBar />
-      </div>
-      <div>
+        <>
         <Routes>
           <Route exact path="/" element={<Home/>} />
           <Route exact path='/Projects' element={<Projects />} />
         </Routes>
-      </div>
+        </>
       </BrowserRouter>
       </>
   );
--- test/src/components/Contact.js
+++ test/src/components/Contact.js
@@ -4,7 +4,7 @@ let gitHub = "https://github.com/Ceaseless04";
 
 export default function Contact() {
     return (
-        <div className="flex container bg-red-200 my-11 w-auto text-center" id="contact"> {/* this will be the card container */}
+        <div className="bg-red-200 my-11 w-auto text-center" id="contact"> {/* this will be the card container */}
           <ul>
             <li>
               <a href={linkedIn} target="_blank" rel="noreferrer" className='block px-4 py-3.5 hover:bg-red-300'>LinkedIn</a>
